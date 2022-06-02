26d2c044070af99175b0911168e0a5add02f0a93
Tu NombreJosé <you@example.comjestebanl@uoc.edu>
2022-06-02 19:21:55 +0200

    Primer envio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [ './src/**/*.{html,js}' ],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
